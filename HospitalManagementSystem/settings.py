4a26dba819bde3e3b1512e3323767c9b12e8ce25
Wamaitha <wamaithaweru19@gmail.com>
2022-06-24 11:08:12 +0300

    edit templates

--- HospitalManagementSystem/settings.py
+++ HospitalManagementSystem/settings.py
@@ -173,11 +173,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email configurations remember to install python-decouple
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='webprojects51@gmail.com'
+EMAIL_HOST_PASSWORD='oaklioasfwkmfsev'
 
 # DEFAULT_FROM_EMAIL = 'webprojects51@example.com'
 # EMAIL_USE_SSL=False
